381aa2d62ad4adc77c78ae41630ba9103a658a5f
hill <hill@hilll.dev>
2024-02-18 19:08:07 +0000

    move stuff around

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-cargo build --release
+#cargo build --release
 #cargo build --release --features wasm-bindgen
 cd tuning_systems-wasm
 wasm-pack build --target web --release
-cd www
+cd ../www
 npm install
 tsc
 npm run build
