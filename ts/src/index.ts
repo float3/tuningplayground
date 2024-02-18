b049f1b8bd7bee8575fd94a67ca02e0477bf8afc
hill <hill@hilll.dev>
2024-02-18 21:01:50 +0000

    now we can start writing typescript again bruh

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,4 +1,4 @@
 import * as tuning_systems from "tuning_systems-wasm";
 
-let test = tuning_systems.get_ratio("EqualTemperament", 3, 2);
-console.log(test);
+// let test = tuning_systems.get_ratio("EqualTemperament", 3, 2);
+// console.log(test);
