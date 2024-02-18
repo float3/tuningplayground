c7a751c304e33f423d7e788afd6fc2d9d94839e4
hill <hill@hilll.dev>
2024-02-18 22:56:55 +0000

    i'm so happy this worked

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-#cargo build --release
-#cargo build --release --features wasm-bindgen
 cd ./tuning_systems-wasm
-wasm-pack build --target web --profile wasm-release
+wasm-pack build --target web --dev --profile wasm-release
 
 cd ../ts
 npm install
--- tuning_systems-wasm/src/lib.rs
+++ tuning_systems-wasm/src/lib.rs
@@ -1,6 +1,16 @@
 use tuning_systems::{self, TuningSystem};
 use wasm_bindgen::prelude::*;
 
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[allow(dead_code)]
+pub fn set_panic_hook() {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+}
+
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
