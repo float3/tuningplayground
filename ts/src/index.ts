bced486884aa875429621b86c0214a245f2d2feb
hill <hill@hilll.dev>
2024-02-20 12:17:00 +0000

    this commit plays with my node configs

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,6 +1,5 @@
 console.debug('imports');
 import init, * as playground from 'playground';
-import * as Tone from 'tone';
 
 init(); //init wasm IMPORTANT
 
@@ -13,6 +12,7 @@ if (navigator.requestMIDIAccess) {
 
 const octave_size = document.getElementById('octave_size') as HTMLInputElement;
 const tuning_select = document.getElementById('tuning_select') as HTMLSelectElement;
+const volumeSlider = document.getElementById('volumeSlider') as HTMLInputElement;
 
 octave_size.onchange = () => {
 	playground.set_octave_size(parseInt(octave_size.value));
@@ -52,29 +52,62 @@ function on_midi_message(event: WebMidi.MIDIMessageEvent) {
 
 document.addEventListener('keydown', function (event) {
 	console.debug('keydown');
+	if (!document.hasFocus()) return;
+	if (!(event.code in keyboard)) return;
+	if (event.repeat) return;
+
 	if (document.activeElement?.tagName === 'BODY') {
 		// if (recording) { }
+		const tone_index: number = keyboard[event.code] + 24 + 24;
+		note_on(tone_index);
 	}
 });
 
 document.addEventListener('keyup', function (event) {
 	console.debug('keyup');
+	if (!(event.code in keyboard)) return;
+
 	if (document.activeElement?.tagName === 'BODY') {
 		// if (recording) { }
-		const tone_index: number = keyboard[event.code] + 24;
+		const tone_index: number = keyboard[event.code] + 24 + 24;
 		note_off(tone_index);
 	}
 });
 
+const playing_tones: Record<number, OscillatorNode> = [];
+
 function note_on(tone_index: number) {
 	console.debug('note_on');
-	console.log(playground.get_frequency(tuning_select.value, tone_index));
+	const freq = playground.get_frequency(tuning_select.value, tone_index);
+	console.debug('tone_index', tone_index);
+	console.debug('freq', freq);
+	playFrequencyNative(freq, parseFloat(volumeSlider.value), tone_index);
 }
 
 function note_off(tone_index: number) {
 	console.debug('note_off');
-	console.log(playground.get_frequency(tuning_select.value, tone_index));
+	playing_tones[tone_index].stop();
 }
+
+let audioContext: AudioContext;
+
+function playFrequencyNative(
+	frequency: number,
+	volume: number,
+	tone_index: number
+): void {
+	audioContext = new window.AudioContext();
+	const oscillator = audioContext.createOscillator();
+	const gainNode = audioContext.createGain();
+	gainNode.gain.value = volume;
+	gainNode.connect(audioContext.destination);
+	oscillator.type = 'square'; // TODO: make this configurable
+	oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+	oscillator.connect(gainNode);
+	oscillator.start();
+	playing_tones[tone_index] = oscillator;
+}
+
 
 const keyboard: Record<string, number> = {
 	//TODO: adjust this to match real DAW keymaps and maybe detect keymap and switch between different layouts
