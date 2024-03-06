25d4c01393b8fd33e5b43d95999689dac04a01ec
float3 <float3@users.noreply.github.com>
2024-03-06 15:51:35 +0000

    actions

--- ts/src/events.ts
+++ ts/src/events.ts
@@ -1,32 +1,32 @@
-import * as wasm from 'wasm';
-import { heldKeys, noteOn, noteOff } from '.';
-import { stopAllTones } from '.';
+import * as wasm from "wasm";
+import { heldKeys, noteOn, noteOff } from ".";
+import { stopAllTones } from ".";
 
 export function visibilityChange(): void {
-	console.debug('visibilityChange');
-	if (document.hidden) {
-		stopAllTones();
-	}
+  console.debug("visibilityChange");
+  if (document.hidden) {
+    stopAllTones();
+  }
 }
 
 export function keydown(event: KeyboardEvent): void {
-	console.debug('keydown');
-	if (!document.hasFocus()) return;
-	if (event.repeat) return;
-	if (event.code in heldKeys) return;
+  console.debug("keydown");
+  if (!document.hasFocus()) return;
+  if (event.repeat) return;
+  if (event.code in heldKeys) return;
 
-	if (document.activeElement?.tagName === 'BODY') {
-		// if (recording) { }
-		const tone_index: number = wasm.from_keymap(event.code);
-		noteOn(tone_index);
-		heldKeys[event.code] = true;
-	}
+  if (document.activeElement?.tagName === "BODY") {
+    // if (recording) { }
+    const tone_index: number = wasm.from_keymap(event.code);
+    noteOn(tone_index);
+    heldKeys[event.code] = true;
+  }
 }
 
 export function keyup(event: KeyboardEvent): void {
-	console.debug('keyup');
-	// if (recording) { }
-	const tone_index: number = wasm.from_keymap(event.code);
-	noteOff(tone_index);
-	delete heldKeys[event.code];
+  console.debug("keyup");
+  // if (recording) { }
+  const tone_index: number = wasm.from_keymap(event.code);
+  noteOff(tone_index);
+  delete heldKeys[event.code];
 }
