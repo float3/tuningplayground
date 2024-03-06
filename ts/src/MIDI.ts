fa1de1bfa20d6774e0a903bca8652bfaaa1b8819
hill <hill@hilll.dev>
2024-03-06 16:37:17 +0100

    testetset

--- ts/src/MIDI.ts
+++ ts/src/MIDI.ts
@@ -1,44 +1,44 @@
-import { noteOn, noteOff } from ".";
+import { noteOn, noteOff } from '.';
 
 export function requestMIDI(): void {
-  if (navigator.requestMIDIAccess) {
-    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-  } else {
-    alert("WebMIDI is not supported in this browser.");
-  }
-  if (navigator.requestMIDIAccess) {
-    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-  } else {
-    alert("WebMIDI is not supported in this browser.");
-  }
+	if (navigator.requestMIDIAccess) {
+		navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+	} else {
+		alert('WebMIDI is not supported in this browser.');
+	}
+	if (navigator.requestMIDIAccess) {
+		navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+	} else {
+		alert('WebMIDI is not supported in this browser.');
+	}
 }
 
 function onMIDISuccess(midiAccess: WebMidi.MIDIAccess): void {
-  console.debug("onMIDISuccess");
-  const input = midiAccess.inputs.values().next().value;
+	console.debug('onMIDISuccess');
+	const input = midiAccess.inputs.values().next().value;
 
-  if (input) {
-    input.onmidimessage = onMIDIMessage;
-  } else {
-    alert("No MIDI input devices found.");
-  }
+	if (input) {
+		input.onmidimessage = onMIDIMessage;
+	} else {
+		alert('No MIDI input devices found.');
+	}
 }
 
 function onMIDIFailure(error: DOMException): void {
-  console.debug("onMIDIFailure");
-  console.error("MIDI Access failed:", error);
+	console.debug('onMIDIFailure');
+	console.error('MIDI Access failed:', error);
 }
 
 function onMIDIMessage(event: WebMidi.MIDIMessageEvent): void {
-  console.debug("onMIDIMessage");
-  const [status, tone_index, velocity] = event.data;
-  const is_note_on = (status & 240) === 144;
-  const is_note_off = (status & 240) === 128;
+	console.debug('onMIDIMessage');
+	const [status, tone_index, velocity] = event.data;
+	const is_note_on = (status & 240) === 144;
+	const is_note_off = (status & 240) === 128;
 
-  if (is_note_off) {
-    noteOff(tone_index);
-  }
-  if (is_note_on) {
-    noteOn(tone_index, velocity);
-  }
+	if (is_note_off) {
+		noteOff(tone_index);
+	}
+	if (is_note_on) {
+		noteOn(tone_index, velocity);
+	}
 }
--- ts/src/Tone.ts
+++ ts/src/Tone.ts
@@ -1,22 +1,22 @@
 export class Tone {
-  index: number;
-  freq: number;
-  cents: number;
-  name: string;
-  Oscillator: OscillatorNode;
-  constructor(
-    index: number,
-    freq: number,
-    cents: number,
-    name: string,
-    node: OscillatorNode,
-  ) {
-    this.index = index;
-    this.freq = freq;
-    this.cents = cents;
-    this.name = name;
-    this.Oscillator = node;
-  }
+	index: number;
+	freq: number;
+	cents: number;
+	name: string;
+	Oscillator: OscillatorNode;
+	constructor(
+		index: number,
+		freq: number,
+		cents: number,
+		name: string,
+		node: OscillatorNode,
+	) {
+		this.index = index;
+		this.freq = freq;
+		this.cents = cents;
+		this.name = name;
+		this.Oscillator = node;
+	}
 }
 
 declare global {
@@ -32,12 +32,12 @@ declare global {
 }
 
 export function createTone(
-  index: number,
-  freq: number,
-  cents: number,
-  name: string,
-  oscillator: OscillatorNode,
+	index: number,
+	freq: number,
+	cents: number,
+	name: string,
+	oscillator: OscillatorNode,
 ): Tone {
-  console.debug("window.createTone");
-  return new Tone(index, freq, cents, name, oscillator);
+	console.debug('window.createTone');
+	return new Tone(index, freq, cents, name, oscillator);
 }
