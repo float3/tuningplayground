98270d0b14fe330214b08f3c2f3c58dfbcd018aa
hill <hill@hilll.dev>
2024-03-12 17:28:40 +0000

    Refactor test.py to use if __name__ == "__main__" and iterate over chord definitions

--- music21-rs/test.py
+++ music21-rs/test.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-sys.path.append('./music21')
+    sys.path.append("./music21")
+    from music21 import chord
 
-import music21
+    chord_definitions = [
+        "C E G",
+        "C E G B",
+        "C E G B D",
+        "C E G B D F",
+        "C E G B D F A",
+        "F A C E",
+        "G B D F",
+        "A C E G",
+        "D F A C",
+        "E G# B D#",
+        "A C# E G#",
+        "B D# F# A#",
+        "F# A# C# E# G#",
+        "D F# A",
+        "E G B",
+        "A C# E",
+        "B D# F#",
+        "C# E# G#",
+        "F# A# C#",
+        "G# B# D#",
+        "A# C## E#",
+        "D G B",
+        "E A C#",
+        "F# B D#",
+        "G# C# F",
+        "A# D# G",
+        "C F A",
+    ]
 
-# here are a few common chords
-chord = music21.chord.Chord("C E G")
-print(chord.pitchedCommonName)
-chord = music21.chord.Chord("C E G B")
-print(chord.pitchedCommonName)
-chord = music21.chord.Chord("C E G B D")
-print(chord.pitchedCommonName)
-chord = music21.chord.Chord("C E G B D F")
-print(chord.pitchedCommonName)
-chord = music21.chord.Chord("C E G B D F A")
-print(chord.pitchedCommonName)
-chord = music21.chord.Chord("C E G B D F A C")
-print(chord.pitchedCommonName)
-chord = music21.chord.Chord("C E G B D F A C E")
-print(chord.pitchedCommonName)
+    for definition in chord_definitions:
+        c = chord.Chord(definition)
+        print(f"{definition}: {c.pitchedCommonName}")
